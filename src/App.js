7dae46100d605ed2a45163de275d8a15d86b2549
Roger Duchesneau <Roger.Duchesneau@carrier.com>
2021-03-11 23:40:01 -0500

    More changes.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import clogo from './carrier-corp-logo.png';
 import logo from './logo.svg';
 import './App.css';
 import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-//import { listNotes } from './graphql/queries';
-//import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 import { listAmplifyNetbox3s } from './graphql/queries';
 import { createAmplifyNetbox3 as createNbMutation, deleteAmplifyNetbox3 as deleteNbMutation } from './graphql/mutations';
 
@@ -12,13 +11,12 @@ import { createAmplifyNetbox3 as createNbMutation, deleteAmplifyNetbox3 as delet
 const initialFormState = { systemIdentifier: 'TESTNETBOXID', clientId: 'TESTCLIENTID' }
 
 function App() {
-//  const [notes, setNotes] = useState([]);
   const [netboxes, setNbs] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
   useEffect(() => {
     fetchNbs();
-  }, [netboxes]);
+  }, []);
 
   async function fetchNbs() {
     const apiData = await API.graphql({ query: listAmplifyNetbox3s });
@@ -36,15 +34,16 @@ function App() {
   }
 
   async function deleteNb({ id }) {
-    const newNbArray = netboxes.filter(netbox => netbox.systemIdentifier !== id);
+    const newNbArray = netboxes.filter(netbox => netbox.id !== id);
     setNbs(newNbArray);
     await API.graphql({ query: deleteNbMutation, variables: { input: { id } }});
   }
 
   return (
     <div className="App">
+      <img src={clogo} className="center" alt="Carrier" />
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>QR Code Access Control</h1>
+      <h1>QR Code Access Control Management Portal</h1>
       <h2>By A-Team</h2>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
@@ -60,7 +59,7 @@ function App() {
       <div style={{marginBottom: 30}}>
         {
           netboxes.map(netbox => (
-            <div key={netbox.systemIdentifier || netbox.clientId}>
+            <div key={netbox.id || netbox.systemIdentifier}>
               <p><b>NetBox: </b>{netbox.systemIdentifier}, 
               {netbox.clientId}</p>
               <button onClick={() => deleteNb(netbox)}>Remove NetBox</button>
